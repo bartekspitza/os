a9d2d2c479f51f6ec1620d31397dd82ec080ad6b
bartekspitza <bartekspitza@hotmail.com>
2026-01-02 12:04:58 +0100

    refactor proc. to use memset

--- src/proc.c
+++ src/proc.c
@@ -1,15 +1,14 @@
 #include "proc.h"
 #include "kalloc.h"
+#include "lib.h"
 #include <stdint.h>
 
 struct proc proc_table[NPROC];
 
 void proc_init(void) {
+    memset(proc_table, 0, sizeof(proc_table));
     for (size_t i = 0; i < NPROC; i++) {
         proc_table[i].state = UNUSED;
-        proc_table[i].kstack = 0;
-        proc_table[i].pid = 0;
-        proc_table[i].tf = 0;
     }
 }
 
@@ -20,19 +19,20 @@ struct proc* allocproc(void) {
     for (size_t i = 0; i < NPROC; i++) {
         if (proc_table[i].state == UNUSED) {
             // Found!
-            p = &proc_table[i];
-            p->pid = 1; // This will be handled down the line
-            p->state = USED;
-
-            // Request a memory page
+            // Immediately request a memory page
             void* mem_page = kalloc();
             if (mem_page == NULL) {
-                // If OOM, rollback the process state
-                p->state = UNUSED;
+                return NULL;
             }
+            memset(mem_page, 0, PAGE_SIZE);
+
+            p = &proc_table[i]; 
+            memset(p, 0, sizeof(*p));
+            p->pid = 1; // @todo
+            p->state = USED;
 
             uintptr_t page_top = (uintptr_t) mem_page + PAGE_SIZE;
-            p->tf = (trapframe_t*) page_top - sizeof(trapframe_t);
+            p->tf = (trapframe_t*) (page_top - sizeof(trapframe_t));
             // kstack can start right where the trapframe starts, because it grows downwards
             p->kstack = (uintptr_t) p->tf;
 
